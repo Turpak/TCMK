e9eab8e234fbe39b18d9c4a09f3cf0897af8210d
Turpak <turpak_rom_15@mail.ru>
2014-06-12 14:25:08 +0800

    Lab 2

--- bigNumber.h
+++ bigNumber.h
@@ -1,33 +1,36 @@
 #pragma once
 #include <iostream>
+#include <vector>
 
 #define BASE 1000000000
-#define DIV_BY_ZERO 1
 
 class bigNumber
 {
 private:
-	int _size;
-	unsigned int* _digits;
 	int _sign;
+	std::vector<unsigned int> _elements;
+
+	unsigned int & operator[](int i);
+	unsigned int operator[](int i) const;
+	void _DelZeroes();
+	long long int _compare(const bigNumber& B);
+	void _shiftLeft(int s);
+
+	bigNumber _sum(const bigNumber& A, const bigNumber& B) const;
+	bigNumber _sub(const bigNumber& A, const bigNumber& B) const;
+	bigNumber _dividing(const bigNumber& A, const bigNumber& B, bigNumber &remainder) const;
 
 public:
 	bigNumber();
-	bigNumber(const char* String);
-	bigNumber(const bigNumber& RightHandValue);
-	bigNumber(long long int RightHandValue);
-	~bigNumber();
+	bigNumber(char* text);
+	bigNumber(long long int value);
 
 	char* getString();
-	char* __str__(); // для вывода числа в Python
-	char* __repr__();
 	bool getNumberFromTextFile(const char* FileName);
 	bool saveNumberToTextFile(const char* FileName);
 	bool getNumberFromBinFile(const char* FileName);
 	bool saveNumberToBinFile(const char* FileName);
 
-
-	bigNumber& operator=(const bigNumber& RightHandValue);
 
 	bigNumber operator+(const bigNumber& right) const;
 	bigNumber operator-() const;
@@ -45,38 +48,7 @@ public:
 	bool operator==(const bigNumber& B);
 	bool operator!=(const bigNumber& B);
 
-	// перегрузка операторов для Python
-	bigNumber operator+(const int& right) const;
-	bigNumber operator-(const int& right) const;
-	bigNumber operator*(const int& right) const;
-	bigNumber operator/(const int& right) const;
-	bigNumber operator%(const int& right) const;
-
-	bool operator>(const int& B);
-	bool operator>=(const int& B);
-	bool operator<(const int& B);
-	bool operator<=(const int& B);
-	bool operator==(const int& B);
-	bool operator!=(const int& B);
-
-
-	friend std::ostream& operator<<(std::ostream &out, bigNumber A);
-	friend std::istream& operator>>(std::istream &is, bigNumber &A);
-
-private:
-	void _setSize(int size);
-	unsigned int & operator[](int i);
-	unsigned int operator[](int i) const;
-	void _copy(const bigNumber &rhv);
-	void _DelZeroes();
-	long long int _compare(const bigNumber& B);
-	void _shiftLeft(int s);
-
-
-	bigNumber _sumAndSub(const bigNumber& left, const bigNumber& right) const;
-	bigNumber _multiplication(const bigNumber A, const bigNumber B) const;
-	bigNumber _dividing(const bigNumber& A, const bigNumber& B, bigNumber &remainder) const;
-
+	
 };
 
 bigNumber Pow(const bigNumber& A, const bigNumber& B, bigNumber& modulus);
